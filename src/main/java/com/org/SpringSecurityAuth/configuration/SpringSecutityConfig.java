1df54bdd21ba695bdced930833a5127a07e4020c
Dione <dioneousseynou03@gmail.com>
2022-08-13 01:31:35 +0000

    fisrt commit

--- src/main/java/com/org/SpringSecurityAuth/configuration/SpringSecutityConfig.java
+++ src/main/java/com/org/SpringSecurityAuth/configuration/SpringSecutityConfig.java
@@ -1,16 +1,40 @@
 package com.org.SpringSecurityAuth.configuration;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //On ajoute l'annotation configuration pour dire à Spring que c'est une classe de configuration.
 @Configuration
 @EnableWebSecurity
 public class SpringSecutityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("springuser").password(passwordEncoder().encode("Spring123")).roles("USER").and()
+                .withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN","USER");
+
+
+    }
+
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USERS")
+                .anyRequest().authenticated().and()
+                .formLogin();
 
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
